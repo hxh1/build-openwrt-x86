06362aa587026cfee96b0d78997bc767a94c6590
歌者 <34333540+hxh1@users.noreply.github.com>
2025-03-29 20:35:14 +0800

    Update diy-part.sh

--- build/Immortalwrt/diy-part.sh
+++ build/Immortalwrt/diy-part.sh
@@ -71,7 +71,7 @@ export kernel_usage="stable"
 
 
 # 修改插件名字
-sed -i 's/"终端"/"TTYD"/g' `egrep "终端" -rl ./`
+#sed -i 's/"终端"/"TTYD"/g' `egrep "终端" -rl ./`
 sed -i 's/"网络存储"/"NAS"/g' `egrep "网络存储" -rl ./`
 sed -i 's/"实时流量监测"/"流量"/g' `egrep "实时流量监测" -rl ./`
 sed -i 's/"KMS 服务器"/"KMS激活"/g' `egrep "KMS 服务器" -rl ./`
