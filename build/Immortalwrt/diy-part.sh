a8bbeea3e37e759c0426ed48c8c9584bb7bedc82
歌者 <34333540+hxh1@users.noreply.github.com>
2026-08-31 22:15:41 +0800

    Update diy-part.sh

--- build/Immortalwrt/diy-part.sh
+++ build/Immortalwrt/diy-part.sh
@@ -32,6 +32,7 @@ export Enable_IPV4_function="0"             # 编译IPV4固件(1为启用命令,
 
 # 替换OpenClash的源码(默认master分支)
 export OpenClash_branch="1"                 # OpenClash的源码分别有【master分支】和【dev分支】(填0为关闭,填1为使用master分支,填2为使用dev分支,填入1或2的时候固件自动增加此插件)
+export OpenClash_Core="1" 
 
 # 个性签名,默认增加年月日[$(TZ=UTC-8 date "+%Y.%m.%d")]
 export Customized_Information="$(TZ=UTC-8 date "+%Y.%m.%d")"  # 个性签名,你想写啥就写啥，(填0为不作修改)
